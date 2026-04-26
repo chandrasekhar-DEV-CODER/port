3cdeeb13e2df4dd1183edd96ad39a014195bc45f
SATHI CHANDRA SEKHAR MANIKANTA REDDY <chandrasekharsathi.dev@gmail.com>
2026-04-26 18:29:51 +0530

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,9 +3,19 @@ import React from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { motion } from "framer-motion";
+import { textVariant } from "../utils/motion";
 
 const Tech = () => {
   return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} text-center`}>
+          What I have done so far
+        </p>
+        <h2 className={`${styles.sectionHeadText} text-center flex items-center justify-center gap-4`}>
+          Work Experience. </h2>
+      </motion.div>
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
@@ -13,6 +23,7 @@ const Tech = () => {
         </div>
       ))}
     </div>
+    </>
   );
 };
 
